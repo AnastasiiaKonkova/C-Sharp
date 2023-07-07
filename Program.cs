68df0b98ee89c0cdc10d04b77983d837a4870f27
Nastya <anastasiya.komova@mail.ru>
2023-07-07 15:25:03 +0300

    задача1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 1. 
+// Необходимо создать два строковых массива (один заполнить, второй для заполнения).
+// Написать цикличный метод, который проверяет каждый эллемент массива на соответствие условию (длинна элемента не более 3 символов), если условие выполняется то элемент вносится в массив 2. Цикл работает до тех пор, пока колличество элемментов в массиве не закончится.
+// Написать метод для вывода полученного массива.
+
+/*
+string[] arrayOne = new string[6] {"530", "083", "end", "world", "hello" , ".,k"};
+string[] arrayTwo = new string[arrayOne.Length];
+
+void newArrayString3(string[] arrayOne, string[] arrayTwo)
+{
+    int count = 0;
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+        if(arrayOne[i].Length < 4)
+        {
+            arrayTwo[count] = arrayOne[i];
+            count++;
+        }
+    }
+}
+
+void outputArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} ");
+Console.WriteLine();
+}
+
+newArrayString3(arrayOne, arrayTwo);
+outputArray(arrayTwo);
+*/
+
+
+
+
+
